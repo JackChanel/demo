068ea760158792c38b3227af212402b4964760e3
jinqihan <jinqihan@hikvision.com.cn>
2022-04-28 15:33:51 +0800

    add UI

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,26 +1,45 @@
-<script>
-</script>
+<script></script>
 
 <template>
   <div>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
     <div class="bg-white w500 h100">233</div>
   </div>
 </template>
 
-<style>
-  @import "./assets/base.css";
+<style lang="scss" scoped>
+@import './assets/base.css';
+$bg-red: rgb(255, 172, 172);
 
-  #app {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 2rem;
+.bg-white{
+  background: $bg-red;
+}
 
-    font-weight: normal;
-  }
+.w500{
+  width: 500px;
+}
 
-  .logo {
-    display: block;
-    margin: 0 auto 2rem;
-  }
+.h100{
+  height: 100px;
+}
+
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
 </style>
