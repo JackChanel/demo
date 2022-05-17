f7e97664aa4d4d36c553ad8e8fb10958bafc703b
jinqihan <jinqihan@hikvision.com.cn>
2022-05-17 13:58:46 +0800

    这题不会啊啊啊

--- 来点算法/剑指 Offer 11. 旋转数组的最小数字.js
+++ 来点算法/剑指 Offer 11. 旋转数组的最小数字.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var minArray = function (numbers) {
-  if (numbers.length = 1) return numbers[0]
+  if (numbers.length == 1) return numbers[0]
   let l = 0;
-  let r = nums.length - 1
+  let r = numbers.length - 1
   let mid
-  while (l > r) {
+  while (l < r) {
     mid = l + Math.floor((r - l) / 2)
-    if (nums[mid + 1] < nums[mid]) return mid + 1
-    if (nums[mid + 1] >= nums[mid]) l = mid + 1
+    if (numbers[mid + 1] < numbers[mid]) return numbers[mid + 1]
+    if (numbers[mid + 1] >= numbers[mid]) l = mid
   }
 };
 
+
 debugger
 console.log(minArray(
-  [2, 2, 2, 0, 1]));
+  [1,3,5]));
